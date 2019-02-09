b1ffad2002008bde0d0899e5d2a6ccdc248173d0
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2019-02-08 17:39:34 -0700

    First commit
    Nothing interesting is here yet, sorry

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nsmbpy',
+    version='1.0.0',
+    author='RoadrunnerWMC',
+    author_email='roadrunnerwmc@gmail.com',
+    description='Python library that can help you read, modify and create file types used in New Super Mario Bros.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RoadrunnerWMC/nsmbpy',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'ndspy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+)
